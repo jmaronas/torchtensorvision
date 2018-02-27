3ecbc76434a3d9f98332800337933ee08e938eee
Juan Maroñas Molano <jmaronasm@gmail.com>
2018-02-27 14:40:45 +0100

    added retransfering to gpu in rotation

--- torchtensorvision/Functional.py
+++ torchtensorvision/Functional.py
@@ -13,12 +13,16 @@ def Zscore(img,mean,std):
 
 
 def rotate(img,angle):#unsupported the rest of torchvision options
+	bool_cuda=False
 	if img.is_cuda:
+		bool_cuda=True
 		img=img.cpu()
 	img=img.numpy()
 	img=numpy.transpose(img,(1,2,0))
 	img=imrotate(img,numpy.float32(angle),mode='symmetric').astype(numpy.float32)
 	img=numpy.transpose(img,(2,0,1))
-	return torch.from_numpy(img)
-	
+	if bool_cuda:
+		return torch.from_numpy(img).cuda()
+	else:
+		return torch.from_numpy(img)
 	
